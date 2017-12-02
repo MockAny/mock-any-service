4e8d734d27eacd6c9f2b5831f6a01839e9ed7e4b
Pedro Furst <pedrofurst@gmail.com>
2017-12-02 00:31:42 -0200

    feat: proof of concept

---
+++ src/main/java/com/mockany/service/MockAnyService.java
@@ -0,0 +1,14 @@
+package com.mockany.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class MockAnyService {
+
+	public static void main(String[] args) {
+		run( MockAnyService.class, args );
+	}
+}
