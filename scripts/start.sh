963e405b806d62a86575538a61bbed2e7cdfc143
Thiago Diniz da Silveira <thiagods.ti@gmail.com>
2017-12-09 20:33:01 -0200

    fix/ trying to fix start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "primeira parte" > primeiro.txt
 cd /home/ubuntu/mockany
+echo "segundo step " > segundo.txt
 sudo java -jar -Xms128m -Xmx256m -Dserver.port=80 -Dspring.data.mongodb.uri=$MONGODB_URI -Dspring.profiles.active=production \
     build/libs/mock-any-service-*.jar > /dev/null 2> /dev/null < /dev/null &
+echo "terceiro step " > terceiro.txt
