da8a2830f0d3203f98415414c9d976c8c11e0c2b
Pedro Furst <pedrofurst@gmail.com>
2017-12-09 18:27:56 -0200

    adding the name of jar

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/mockany
 sudo java -jar -Dserver.port=80 -Dspring.profiles.active=production \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+    build/libs/mock-any-service-*.jar > /dev/null 2> /dev/null < /dev/null &
