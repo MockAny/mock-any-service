b870fc6fc9152b8156176b8499db3f4990bbf080
Thiago Diniz da Silveira <thiagods.ti@gmail.com>
2017-12-09 20:16:56 -0200

    fix/ Xms removed =

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/mockany
-sudo java -jar -Xms=128m -Xmx256m -Dserver.port=80 -Dspring.data.mongodb.uri=$MONGODB_URI -Dspring.profiles.active=production \
+sudo java -jar -Xms128m -Xmx256m -Dserver.port=80 -Dspring.data.mongodb.uri=$MONGODB_URI -Dspring.profiles.active=production \
     build/libs/mock-any-service-*.jar > /dev/null 2> /dev/null < /dev/null &
